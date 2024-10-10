b140e327459197ad723754ede811a6260e0a5100
Lalith Kumar <allulalithkumar77@gmail.com>
2024-10-10 19:02:57 +0530

    Updated server and QR code generation functionality

--- api/submit.js
+++ api/submit.js
@@ -1,20 +1,19 @@
 import express from "express";
 import { fileURLToPath } from "url";
-import { dirname ,join} from "path";
+import { dirname, join } from "path";
 import bodyParser from "body-parser";
 import qr from "qr-image";
 
 const dir = dirname(fileURLToPath(import.meta.url));
-const port = 3000;
 const app = express();
-app.use(express.static(join(dir,'../public')));
+app.use(express.static(join(dir, '../public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(join(dir , "../public/index.html"));
+  res.sendFile(join(dir, '../public/index.html'));
 });
 
-app.post("/submit", (req, res) => {
+app.post("/api/submit", (req, res) => {
   const text = req.body.text;
   const qrCode = qr.image(text, {
     type: "png",
@@ -34,7 +33,11 @@ app.post("/submit", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is Running on `);
-  console.log(`http://localhost:${port}`);
+// Catch-all for 404 errors
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
 });
